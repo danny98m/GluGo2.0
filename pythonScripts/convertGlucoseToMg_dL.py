cb2097865857295505cb5d396445183b27c79da8
danny98m <dpmullen98@gmail.com>
2018-09-25 11:23:01 -0400

    Added convert function

--- pythonScripts/convertGlucoseToMg_dL.py
+++ pythonScripts/convertGlucoseToMg_dL.py
@@ -1,8 +1,11 @@
 # convertGlucoseToMg_dL.py
 # Converts the glucose values from mmol/L to mg/dL
 
-def main():
+def convertGlucValues():
 	pass
+
+def main():
+	convertGlucValues()
 
 if __name__ == '__main__':
 	main()

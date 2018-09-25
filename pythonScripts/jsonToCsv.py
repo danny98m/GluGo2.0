fbfd335ada4257551f4c48d152eefff93be31b4c
danny98m <dpmullen98@gmail.com>
2018-09-24 22:46:59 -0400

    set up file to be properly used as a module

--- pythonScripts/jsonToCsv.py
+++ pythonScripts/jsonToCsv.py
@@ -1,4 +1,9 @@
 import pandas as pd
-df = pd.read_json('test_kates_data.json')
-df.to_csv('test_kates_data.csv')
+
+def main():
+    df = pd.read_json('test_kates_data.json')
+    df.to_csv('test_kates_data.csv')
+
+if __name__ == "__main__":
+    main()
 

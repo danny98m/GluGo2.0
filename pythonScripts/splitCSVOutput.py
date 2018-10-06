a5f424246fca825c2dc6493422936c70fff140e4
danny98m <dpmullen98@gmail.com>
2018-10-05 20:44:03 -0400

    Removed inconsitent tabs and spaces

--- pythonScripts/splitCSVOutput.py
+++ pythonScripts/splitCSVOutput.py
@@ -43,51 +43,49 @@ with open ("gluPredict.csv", mode='r') as fd:
     for row in reader:
         # string time
         time.append(row[0])
-        
+
         # convert to timestamp and store
         convertDate = datetime.datetime.strptime(row[0], '%m/%d/%y %H:%M').timestamp()
         timestamp.append(convertDate)
-        
+
         # glucose predication value
         predict_value.append(row[1])
-        
-        # actual glucose value 
+
+        # actual glucose value
         actual_value.append(row[2])
-        
-        # carb intake 
+
+        # carb intake
         if (len(row[3]) > 0 and row[3] != 0):
             carb_intake.append(row[3])
         else:
             carb_intake.append(0)
 
-# determine which mode has been used 
+# determine which mode has been used
 for index in range(0, len(time)):
-    
     # determine carb intake timestamp
     if (carb_intake[index] != 0):
         mode_1.append([time[index], predict_value[index], actual_value[index], carb_intake[index]])
         carbIntakeTime = timestamp[index]
-        
+
     # determine glucose level predicted using mode 1 and store
     if ((timestamp[index] - carbIntakeTime) <= MODE_1_TIME_RANGE):
         mode_1.append([time[index], predict_value[index], actual_value[index]])
-        
+
     # append data predicted using mode 2
     else:
         mode_2.append([time[index], predict_value[index], actual_value[index]])
-  
-# write to two separate csv files   
+
+# write to two separate csv files
 with open("mode1_result.csv", 'w') as output:
     writer = csv.DictWriter(output, ['Date', 'Predict Value', 'Actual Value','Carb Intake'])
-    writer.writeheader()      
+    writer.writeheader()
     writer = csv.writer(output)
     writer.writerows(mode_1)
-  
+
 with open("mode2_result.csv", 'w') as output:
     writer = csv.DictWriter(output, ['Date', 'Predict Value', 'Actual Value'])
-    writer.writeheader()                      
+    writer.writeheader()
     writer = csv.writer(output)
-    writer.writerows(mode_2)      
-        
-        
+    writer.writerows(mode_2)
+
 print ("\n\nDone.\n")
